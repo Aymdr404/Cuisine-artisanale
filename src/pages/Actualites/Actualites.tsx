aa866d0807e4fe9da1f64bfcd6bd1d335a3d17e5
aymeric.sabatier <aymeric.sabatier@absolut-system.com>
2025-02-24 10:53:59 +0100

    [FIX] Changement actualité

--- src/pages/Actualites/Actualites.tsx
+++ src/pages/Actualites/Actualites.tsx
@@ -5,9 +5,10 @@ import Actualite from '@/components/Actualite/Actualite';
 const Actualites: React.FC = () => {
   return (
     <div className="Actualites">
+      <h1>Ici sera bientôt mis en place les actualités culinaires et gastronomiques</h1>
+      {/* <Actualite />
       <Actualite />
-      <Actualite />
-      <Actualite />
+      <Actualite /> */}
     </div>
   );
 };
