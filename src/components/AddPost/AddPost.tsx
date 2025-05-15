c45dd084907d611c693fcb80a37cd9b74311bc93
Aymeric Sabatier <ssabatieraymeric@gmail.com>
2025-05-15 18:46:55 +0200

    modif posts

--- src/components/AddPost/AddPost.tsx
+++ src/components/AddPost/AddPost.tsx
@@ -3,6 +3,7 @@ import './AddPost.css';
 import { Button } from 'primereact/button';
 import AddPostForm from '@components/AddPostForm/AddPostForm';
 import { useAuth } from '@contexts/AuthContext/AuthContext';
+import { toast } from 'react-toastify';
 
 const AddPost: React.FC = () => {
   const { user } = useAuth();
@@ -11,27 +12,34 @@ const AddPost: React.FC = () => {
   const openForm = () => setShowForm(true);
   const closeForm = () => setShowForm(false);
 
+  const handleMobileClick = () => {
+    if (user) {
+      openForm();
+    } else {
+      toast.info('Vous devez être connecté pour publier un post.');
+    }
+  };
+
   return (
     <div className="AddPost">
       {/* Desktop layout */}
       <div className="desktop-post">
-        <h2>Add Post</h2>
-        <p>Share your thoughts with the world!</p>
+        <h2>Ajouter un post</h2>
+        <p>Partagez vos pensées avec le monde !</p>
         <Button onClick={openForm} disabled={!user}>
-          Add Post
+          Ajouter un post
         </Button>
-        {!user && <p>You need to be logged in to add a post</p>}
+        {!user && <p>Vous devez être connecté pour ajouter un post</p>}
       </div>
 
       {/* Floating mobile button */}
       <div className="mobile-floating-button">
         <Button
           icon="pi pi-plus"
-          rounded
-          severity="secondary"
-          aria-label="Add"
-          onClick={openForm}
-          disabled={!user}
+          aria-label="Ajouter un post"
+          onClick={handleMobileClick}
+          className="p-button-rounded p-button-lg"
+          text
         />
       </div>
 
